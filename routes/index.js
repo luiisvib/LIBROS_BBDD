4f74c2d42cdb78ef0dadedb0be85637bac7f6c0d
Luis Iñigo <luisvib2003@gmail.com>
2023-01-29 14:05:08 +0100

    Modificacion archivo texto

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var datosjson = require("../datos.json")
+
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs')
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) { 
-  res.render('index', { datos: datosjson, title: "NodeBooks" });
+
+  fs.readFile('datos.txt', (error, respuesta) => {
+    if (error){
+      console.log(error) 
+    }
+    else{
+      var datosString = respuesta.toString()
+      datos = JSON.parse(datosString)
+      datosjson = datos
+      res.render('index', { datos: datos, title: "NodeBooks" });
+    }
+  })
+  
 });
 
 router.get('/addBooks', function(req, res, next) {
@@ -26,6 +38,7 @@ router.post("/addBooks",  function(req, res, next) {
   console.log(datosjson)
   var datos_txt = JSON.stringify(datosjson)
   console.log(datos_txt)
+
   fs.writeFile('datos.txt', datos_txt, error => {
     if (error){
       console.log(error) 
